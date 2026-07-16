5756da17c353290fe69935e2d04bbd6d101b13cb
rakesh070805 <trakesh070805@gmail.com>
2026-07-16 18:52:53 +0530

    Data type
    Data type is the classification of data that specifies the type of value a variable can hold, such as numbers, text or collection

---
+++ data type/data type.py
@@ -0,0 +1,67 @@
+age = 10
+print(age)#Integer
+print(type(age))
+
+rate = 98.99
+print(rate)#Float
+print(type(rate))
+
+name = "Rakesh"
+print(name)#String
+print(type(name))
+
+is_student = True
+print(is_student)#Boolean
+print(type(is_student))
+
+fruit = ["apple","orange"]
+print(fruit)#List
+print(type(fruit))
+
+num = (10,20,30)
+print(num)#Tuple
+print(type(num))
+
+color = {"red","blue","green"}
+print(color)#set
+print(type(color))
+
+student = {"name": "Rakesh", "age": 20}
+print(student)#Dictionary
+print(type(student))
+
+x= 2+3j
+print(x)#Complex
+print(type(x))
+
+value = None
+print(value)#None
+print(type(value))
+
+
+
+
+
+
+a=input("enter ur name:")
+b=int(input("enter integer:"))
+c=float(input("enter float number:"))
+d=input("enter value:")
+e=complex(input("enter value:"))
+f=complex(input("enter value:"))
+print("string:",a)
+print("integer:",b)
+print("float:",c)
+print("bool:",d>a)
+print("complex:",e+f)
+
+
+
+
+
+
+
+
+
+
+
