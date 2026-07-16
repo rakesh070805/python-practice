6942c1dcb8661e86825059e75eb5fa8dd6bcab58
rakesh070805 <trakesh070805@gmail.com>
2026-07-16 17:13:46 +0530

    store and print a variable
    A variable is a name to store a values in memory. The value can be changed during program execution.

---
+++ variables.py
@@ -0,0 +1,37 @@
+# variables.py
+
+name = "Rakesh"
+print("Name:", name)# String variable
+
+age = 20
+print("Age:", age)# Integer variable
+
+height = 5.8
+print("Height:", height)# Float variable
+
+is_student = True
+print("Student:", is_student)# Boolean variable
+
+a=10
+b=20
+print(a+b)#add two variables
+
+x,y,z=10,20,30
+print(x)
+print(y)
+print(z)#multiple variables
+
+a=b=c=100
+print(a)
+print(b)
+print(c)#same value to multiple variables
+
+
+
+
+
+
+
+
+
+
