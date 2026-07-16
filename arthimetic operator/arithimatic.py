71f8f2f4b51a073ada508923223c62aab9346d2f
rakesh070805 <trakesh070805@gmail.com>
2026-07-16 11:05:26 +0530

    arthmetic

---
+++ arthimetic operator/arithimatic.py
@@ -0,0 +1,12 @@
+a=10
+b=6
+print("Addition:",a+b)
+
+
+a=int(input("enter first value:"))
+b=int(input("enter second value:"))
+print(" Addition total:",a+b)
+print("subtraction:",a-b)
+print("multiplication:",a*b)
+print("division:",a/b)
+print("floor:",a//b) 
