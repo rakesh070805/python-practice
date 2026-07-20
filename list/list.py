c48d761581c66113ae31f6baafd23b6fabc8dad5
rakesh070805 <trakesh070805@gmail.com>
2026-07-20 12:00:21 +0530

    list.py
    A list is a built-in python data type used to store multiple items in a single variable.
    A list is an ordered, mutable collection in Python that allows duplicate values and can store elements of different data types. It is created using square brackets [].

---
+++ list/list.py
@@ -0,0 +1,24 @@
+#syntax list_name=["iteam1","iteam2","iteam3"]
+
+fruits=["apple","banana","mango"]
+print(fruits)
+
+#access list iteam
+fruits=["apple","banana","mango"]
+print(fruits[0])
+print(fruits[1])
+
+#change an iteam
+fruits[1]="orange"
+print(fruits)
+
+#add an iteam
+fruits.append("grapes")
+print(fruits)
+
+#remove an iteam
+fruits.remove("apple")
+print(fruits)
+
+#length of list
+print(len(fruits))
