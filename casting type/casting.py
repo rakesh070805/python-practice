fa8b9d497f803eae658699366db1123a4c9245ff
rakesh070805 <trakesh070805@gmail.com>
2026-07-20 11:44:25 +0530

    casting type implicit and explicit
    casting means converting one date type into another
    implicit- automatic conversion by python
    explicit- manual conversion using functions like int(), float(), str(), bool().

---
+++ casting type/casting.py
@@ -0,0 +1,24 @@
+x="10"
+y=int(x)
+print(y)#convert string to integer
+
+
+x=25
+y=float(x)
+print(y)#convert interger to float
+
+
+x=12.8
+y=int(x)
+print(y)#convert float to interger
+
+
+x=100
+y=str(x)
+print(y)#convert interger to string
+
+
+a=[1,2,3,4]
+b=tuple(a)
+print(b)#@convert list to tuple
+
